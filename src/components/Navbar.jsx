e03d5ed1a27217eaf4c07973b3fc8e1708bd2b73
taibislamdipu <mailtaibislam@gmail.com>
2023-02-05 16:05:50 +0600

    nav done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-100">
+      <div className="flex justify-between items-center w-full h-full container mx-auto testb md:px-8 px-4">
+        <div>
+          <Image
+            src="/../public/assets/logo.png"
+            alt=""
+            width="141"
+            height="38"
+          />
+        </div>
+        <div>
+          <ul className="hidden md:flex items-center space-x-9">
+            <Link href="/">
+              <li className="text-black">Home</li>
+            </Link>
+            <Link href="#">
+              <li className="text-black">Program</li>
+            </Link>
+            <Link href="#">
+              <li className="text-black">Blog</li>
+            </Link>
+            <Link href="#">
+              <li className="text-black">About us</li>
+            </Link>
+            <Link href="#">
+              <li className="text-white bg-blue rounded-[10px] py-2 px-7">
+                Log in
+              </li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={24} />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500"
+              : "fixed left-[-100%] h-screen top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div className="space-y-4">
+            <div className="flex justify-between w-full items-center border-b py-4">
+              <Image
+                src="/../public/assets/logo.png"
+                alt=""
+                width="141"
+                height="38"
+              />
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <ul className="space-x-9">
+                <Link href="/">
+                  <li className="text-black">Home</li>
+                </Link>
+                <Link href="#">
+                  <li className="text-black">Program</li>
+                </Link>
+                <Link href="#">
+                  <li className="text-black">Blog</li>
+                </Link>
+                <Link href="#">
+                  <li className="text-black">About us</li>
+                </Link>
+                <Link href="/">
+                  <li className="text-white bg-blue rounded-[10px] py-2 px-7 w-fit">
+                    Log in
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#264373",
+      },
+    },
   },
   plugins: [],
 };
